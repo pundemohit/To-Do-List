47de16c731d6c4c778c2c884eb55327834bbce63
Mohit <pundemohit@gmail.com>
2022-11-15 15:06:11 +0530

    ToDo list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
+import ToDoList from "./ToDoList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+const App = () => {
+
+  const [inputList, setInputList]= useState("");
+  const [Items, setItems]= useState([]);
+
+  
+  const itemEvent = (event) =>{
+    setInputList(event.target.value);
+  };
+
+  const listOfItems = () => {
+    setItems((oldItems) => {
+      return [...oldItems,inputList];
+    });
+    setInputList("");
+  };
+
+  const deleteItems= (id)=>{
+    console.log("aaaa");
+    setItems((oldItems)=> {
+      return oldItems.filter((arrElem, index)=>{
+        return index !== id; 
+      })
+    })
+};
+
+
+
+  return(
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1> ToDo List</h1>
+          <br />
+          <input type="text" placeholder='Add a Items' value={inputList} onChange={itemEvent} />
+          <button onClick={listOfItems} > + </button>
+
+          <ol>
+            {/*<li>{inputList}</li> */}
+            {
+            Items.map((itemval, index) => {
+              return <ToDoList  
+              key={index} 
+              text={itemval} 
+              id={index}
+              onSelect={deleteItems} />;
+            })
+            }
+          </ol>
+
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
+
+
+
+
+
+
+// import logo from './logo.svg';
+
+// function App() {
+//   return (
+//    <div>
+//     <h1>Hello World!</h1>
+//    </div>
+//   );
+// }
+
+// export default App;
